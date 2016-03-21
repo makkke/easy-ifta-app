48d5edff3eef3a02cde44f4180a247ad9ab35199
Viacheslav Ivanov <slava.eth@gmail.com>
2016-03-20 21:44:08 -0700

    fixed npm command execution

--- build-pipeline/build/entrypoint.sh
+++ build-pipeline/build/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 set -e
 
-npm build
-npm start:prod
+function check_port() {
+  local host=${1} && shift
+  local port=${1} && shift
+  local retries=90
+  local wait=1
+
+  until( $(nc -zv ${host} ${port}) ); do
+    ((retries--))
+    if [ $retries -lt 0 ]; then
+      echo "Service ${host} didn't become ready in time."
+      exit 1
+    fi
+    sleep "${wait}"
+  done
+}
+
+check_port "db" "27017"
+
+npm run build
+npm run start:prod
