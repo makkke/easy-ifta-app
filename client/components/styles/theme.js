17de832bfa88efff4daa4ecc7fb85e21c03e46fd
Svetlana Ivanova <svetlana.ivanova0509@gmail.com>
2016-08-10 21:24:03 -0700

    update fonts and colors

--- client/components/styles/theme.js
+++ client/components/styles/theme.js
@@ -11,12 +11,12 @@ export default {
     white: 'rgba(255, 255, 255, 1)',
     white65: 'rgba(255, 255, 255, .65)',
 
-    black: 'rgba(36, 37, 72, 1)',
-    black54: 'rgba(36, 37, 72, .54)',
-    black38: 'rgba(36, 37, 72, .38)',
-    black20: 'rgba(36, 37, 72, .2)',
-    black10: 'rgba(36, 37, 72, .1)',
-    black5: 'rgba(36, 37, 72, .5)',
+    black: 'rgba(51, 60, 78, 1)',
+    black54: 'rgba(51, 60, 78, .54)',
+    black38: 'rgba(51, 60, 78, .38)',
+    black20: 'rgba(51, 60, 78, .2)',
+    black10: 'rgba(51, 60, 78, .1)',
+    black5: 'rgba(51, 60, 78, .5)',
 
     blue: '#3498db',
   },
