6552400efcc328b8de4071c8750951e025bd0245
Svetlana Ivanova <svetlana.ivanova0509@gmail.com>
2016-08-11 18:52:15 -0700

    mixed commit

--- client/modules/auth/LoginPage.js
+++ client/modules/auth/LoginPage.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { ButtonToolbar, Button } from 'react-bootstrap'
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap'
+import theme from '../../components/styles/theme'
+
+function getStyle() {
+  return {}
+}
 
 class LoginPage extends Component {
   static propTypes = {
@@ -13,12 +18,12 @@ class LoginPage extends Component {
     // const { auth } = this.props
 
     return (
-      <div>
-        <h2>Login</h2>
-        <ButtonToolbar>
-          <Button bsStyle="primary">Login</Button>
-        </ButtonToolbar>
-      </div>
+      <form>
+        <FormGroup controlId="email">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl type="text" placeholder="popa" />
+        </FormGroup>
+      </form>
     )
   }
 }
