3b4a4a20992d169d42ae74a861602c4a8562b785
Viacheslav Ivanov <slava.eth@gmail.com>
2016-08-30 22:24:09 -0700

    chore: bump version to 0.2.0

--- client/modules/taxReturn/__test__/utils.spec.js
+++ client/modules/taxReturn/__test__/utils.spec.js
@@ -9,14 +9,3 @@ test('should return latest tax period', t => {
   period = utils.latestTaxPeriod(new Date(2017, 1))
   t.deepEqual(period, { year: 2016, quarter: 4 })
 })
-
-// test('sends the body', t => {
-//   const body = { id: 5 }
-//   const reply = { foo: 'bar' }
-//   nock(API_URL)
-//     .post('/foo', body)
-//     .reply(200, reply)
-//   return callApi('foo', 'post', body).then(response => {
-//     t.deepEqual(response, reply)
-//   })
-// })
